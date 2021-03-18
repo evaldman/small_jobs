d281d91fc831d189b43e716ac147aa84f0b03c60
github username <github email address>
2021-03-18 12:03:57 -0400

    navbar

---
+++ src/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Browse",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign up",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign in",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign Out",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
---
+++ src/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { MenuItems } from "./MenuItems";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
+import { Button } from "./Button";
+
+function Navbar() {
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    setClicked(!clicked);
+  }
+  return (
+    <nav className="navbar-items">
+      <h1 className="navbar-logo">Small Jobs</h1>
+      <div className="menu-icon" onClick={handleClick}>
+        <Link to="#">{clicked ? <FaTimes /> : <FaBars />}</Link>
+      </div>
+      <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <Button>Sign Out</Button>
+    </nav>
+  );
+}
+
+export default Navbar;
