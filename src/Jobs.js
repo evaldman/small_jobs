487cf8d0bc60be53f70ad1a61844852db7894bd0
github username <github email address>
2021-03-24 00:17:41 -0400

    filter

--- src/Jobs.js
+++ src/Jobs.js
@@ -1,37 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Job from "./Job";
 
 function Jobs({ currentUser, jobs, categories }) {
+  const [selected, setSelected] = useState("");
   // console.log(categories);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/jobs")
-  //     .then((response) => response.json())
-  //     .then((jobsData) => setJobs(jobsData));
-  // }, []);
-
-  const openJobs = jobs.filter((job) => job.accept_status !== true);
+  // console.log(selected);
+  const openJobs = jobs.filter((job) => job.accept_status === false);
   const jobsToDisplay = openJobs.map((job) => {
     return <Job key={job.id} job={job} />;
   });
 
   const categorySelect = categories.map((category) => {
+    // console.log(category);
     return (
-      <option key={category.id} value={category.jobs.title}>
+      <option key={category.id} value={category.jobs.map((job) => job.title)}>
         {" "}
         {category.name}
       </option>
     );
   });
 
+  function handleSelected(e) {
+    setSelected(e.target.value);
+  }
+
   return (
     <div>
-      <select>
+      <select onChange={handleSelected}>
         <option value hidden>
           Select Job Category
         </option>
         {categorySelect}
       </select>
+      <button>Sort</button>
       <h3>Welcome {currentUser.name}</h3>
       {jobsToDisplay}
     </div>
